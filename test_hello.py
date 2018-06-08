e56b24586965d8504082926b453a60a2c2e53724
Brad Martin <brad.martin@cfacorp.com>
2018-06-08 12:54:02 -0700

    Making update with string format practice

--- test_hello.py
+++ test_hello.py
@@ -1,2 +1,9 @@
 #!/usr/bin/env python3
+from __future__ import print_function, unicode_literals
+ip_addr = '192.168.10.101'
+octets = ip_addr.split(".")
 print ("Hello World")
+print ("-" * 80)
+print ("{:20}{:20}{:20}{:20}".format(octets[0], octets[1], octets[2], octets[3]))
+#print (octets)
+print ("-" * 80)
